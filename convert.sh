92abc8b0148ba0b1cd83217ff8607657dc6afbff
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2024-10-07 14:35:53 -0300

    with mobile nav

--- convert.sh
+++ convert.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Create the 'resized' directory if it doesn't exist
-rm -f resized/*.jpg
+rm -rf resized
 mkdir -p resized
 
 # Loop through all image files in the current directory
-for img in *.{jpeg,HEIC,png,jpg}; do
+for img in `ls *.jpg(N) *.png(N) *.HEIC(N)`; do 
     # Check if the file exists (in case there are no matching files)
    if [ -e "$img" ]; then
         # Define the output filename for the resized image
         output="resized/${img%.*}.jpg"
 
         # Convert to JPG and resize
-        convert "$img" -resize "2000x1200>" "$output"
+        magick "$img" -resize "2000x1200>" "$output"
         echo "Converted and resized: $img -> $output"
 
         # Generate thumbnail
         thumbnail="resized/${img%.*}-tn.jpg"
-        # convert "$output" -resize "300x300" "$thumbnail"
-        convert "$output" -resize 300x300^ -gravity center -crop 300x300+0+0 +repage "$thumbnail"
+        magick "$output" -resize 600x600^ -gravity center -crop 600x600+0+0 +repage "$thumbnail"
         echo "Thumbnail created: $thumbnail"
     fi
 done
